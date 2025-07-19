d32c899e1b4b129a3aac74244b91f28f4920e9bc
taseenjeem <taseenjeem.dev@gmail.com>
2025-07-19 22:23:51 +0600

    Added CredibilityShowcase

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import CredibilityShowcase from "@/components/pages/landing-page/CredibilityShowcase";
 import HeroSection from "@/components/pages/landing-page/Hero";
 
 export default function LandingPage() {
   return (
     <>
       <HeroSection />
+      <CredibilityShowcase />
     </>
   );
 }
