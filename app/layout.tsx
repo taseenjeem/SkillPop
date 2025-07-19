36b68cd639bc51926ba931880f7874b70b543c9c
taseenjeem <taseenjeem.dev@gmail.com>
2025-07-20 00:19:32 +0600

    changed the default theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
         <body className={`${fontStyle.className} antialiased`}>
           <ThemeProvider
             attribute="class"
-            defaultTheme="system"
+            defaultTheme="dark"
             enableSystem
             disableTransitionOnChange
           >
